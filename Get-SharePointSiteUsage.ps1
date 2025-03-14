dfb6f1086d516814eb15d76f069f1cf30281da5e
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:53:22 -0600

    Update Get-SharePointSiteUsage.ps1

--- Get-SharePointSiteUsage.ps1
+++ Get-SharePointSiteUsage.ps1
@@ -1,5 +1,12 @@
+You can retrieve information about the usage of SharePoint Online sites.
+
+Example: Get SharePoint Site Usage:
+
 # Connect to SharePoint Online Service
 Connect-SPOService -Url "https://yourtenant-admin.sharepoint.com"
 
 # Get Site Usage
 Get-SPOSite | Select-Object URL, StorageUsageCurrent, StorageQuota
+
+This provides a report on SharePoint site usage, including storage usage and storage quota.
+
