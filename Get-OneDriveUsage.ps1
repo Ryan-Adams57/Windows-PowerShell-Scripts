7bb50de37706a72a4ab0d5d4f9a9a3d51ac1a2eb
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:50:12 -0600

    Update Get-OneDriveUsage.ps1

--- Get-OneDriveUsage.ps1
+++ Get-OneDriveUsage.ps1
@@ -1,5 +1,11 @@
+If you're using OneDrive for Business, you can report on user storage.
+
+Example: Get OneDrive for Business Usage:
+
 # Connect to SharePoint Online (for OneDrive)
 Connect-SPOService -Url https://yourtenant-admin.sharepoint.com
 
 # Get OneDrive usage
 Get-SPOSite -Template "SPSPERS" | Select-Object URL, StorageUsageCurrent, StorageQuota
+
+This will give you a report of URL, StorageUsageCurrent, and StorageQuota for all OneDrive for Business sites.
