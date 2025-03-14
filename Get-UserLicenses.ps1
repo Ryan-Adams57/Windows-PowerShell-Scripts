044e7066a7329c78c552c295460ca4da975a4550
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:46:39 -0600

    Update Get-UserLicenses.ps1

--- Get-UserLicenses.ps1
+++ Get-UserLicenses.ps1
@@ -1,1 +1,7 @@
+If you want to see what licenses are assigned to your users, you can use the following command.
+
+Example: Get License Information:
+
 Get-MsolUser | Select-Object UserPrincipalName, Licenses
+
+This will show which licenses are assigned to each user in your Office 365 tenant.
