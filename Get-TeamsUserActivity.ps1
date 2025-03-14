f8f425cf2ef52aee1da74262fe7769941e548dd2
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:51:05 -0600

    Update Get-TeamsUserActivity.ps1

--- Get-TeamsUserActivity.ps1
+++ Get-TeamsUserActivity.ps1
@@ -1,3 +1,7 @@
+If you want to monitor Teams usage and activity, you can get reports related to Teams activity.
+
+Example: Get Teams User Activity:
+
 # Install Teams PowerShell Module
 Install-Module -Name PowerShellGet -Force -AllowClobber
 Install-Module -Name MicrosoftTeams
@@ -7,3 +11,5 @@ Connect-MicrosoftTeams
 
 # Get Teams Activity Report
 Get-TeamUserActivity -StartDate "2025-01-01" -EndDate "2025-01-31"
+
+This retrieves user activity within Teams for a specific date range.
