a4320a86f5ff525971bfdc9b335b4175a99add35
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:52:38 -0600

    Update Search-AuditLogs.ps1

--- Search-AuditLogs.ps1
+++ Search-AuditLogs.ps1
@@ -1,5 +1,11 @@
+You can use PowerShell to pull audit logs for activities such as login attempts, document sharing, and more.
+
+Example: Search Unified Audit Logs:
+
 # Connect to Security & Compliance Center
 Connect-ComplianceCenterPowerShell -UserPrincipalName your-admin@domain.com
 
 # Search Audit Logs
 Search-UnifiedAuditLog -StartDate "2025-01-01" -EndDate "2025-01-31" -Operations FileAccessed | Export-Csv "C:\Reports\AuditLogReport.csv" -NoTypeInformation
+
+This script searches the unified audit logs for the operation FileAccessed between two dates and exports the results to a CSV.
