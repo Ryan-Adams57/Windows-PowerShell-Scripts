bcf82373befe4249d585b44853c57f33af6cb483
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:48:34 -0600

    Update Get-MailboxStatus.ps1

--- Get-MailboxStatus.ps1
+++ Get-MailboxStatus.ps1
@@ -1,1 +1,5 @@
+Example: Get Mailbox Status:
+
 Get-Mailbox -ResultSize Unlimited | Select-Object DisplayName, ArchiveStatus
+
+This will show you the DisplayName and ArchiveStatus (whether a mailbox has an archive enabled).
