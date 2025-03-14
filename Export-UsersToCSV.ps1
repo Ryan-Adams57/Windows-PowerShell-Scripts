15994efdb875734e637fe33fca8b879399ebbd29
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:51:43 -0600

    Update Export-UsersToCSV.ps1

--- Export-UsersToCSV.ps1
+++ Export-UsersToCSV.ps1
@@ -1,1 +1,7 @@
+For all of the above scripts, you can export the data to a CSV file for further analysis or reporting purposes.
+
+Example: Export to CSV:
+
 Get-MsolUser | Select-Object UserPrincipalName, DisplayName, IsLicensed | Export-Csv -Path "C:\Reports\Office365Users.csv" -NoTypeInformation
+
+This script exports a list of all users (including their UserPrincipalName, DisplayName, and IsLicensed) to a CSV file located at C:\Reports\Office365Users.csv.
