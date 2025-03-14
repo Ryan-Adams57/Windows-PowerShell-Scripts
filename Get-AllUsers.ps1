0a27a553f8d1f5b32fc9746e34fc5f171d22ee4b
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2025-03-13 19:45:48 -0600

    Update Get-AllUsers.ps1

--- Get-AllUsers.ps1
+++ Get-AllUsers.ps1
@@ -1,1 +1,5 @@
+Example: List all users in your organization.
+
 Get-MsolUser | Select-Object UserPrincipalName, DisplayName, IsLicensed
+
+This script retrieves all users in your Office 365 tenant and displays their UserPrincipalName, DisplayName, and whether they are licensed or not.
