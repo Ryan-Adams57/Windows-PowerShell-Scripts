e92e055f278125045fbf9fe76ad9923b641856f3
Ryan Adams <106103574+Ryan-Adams57@users.noreply.github.com>
2026-02-24 14:31:16 -0700

    Add comparison operators examples in PowerShell

---
+++ Comparison_Operators.ps1
@@ -0,0 +1,17 @@
+# Greater than
+if (5 -gt 3) { Write-Host "5 is greater than 3" }
+
+# Less than
+if (2 -lt 4) { Write-Host "2 is less than 4" }
+
+# Greater than or equal
+if (3 -ge 3) { Write-Host "3 is greater than or equal to 3" }
+
+# Less than or equal
+if (2 -le 5) { Write-Host "2 is less than or equal to 5" }
+
+# Equals
+if (7 -eq 7) { Write-Host "7 equals 7" }
+
+# Not equals
+if (5 -ne 3) { Write-Host "5 does not equal 3" }
